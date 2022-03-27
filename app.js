01b94035dfdf0ed6481ac40e5f07605c3fa94152
Jose001R <77294118+Jose001R@users.noreply.github.com>
2022-03-26 21:40:23 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,29 @@
+const rpta = document.getElementById('resultado');
+const botones = document.querySelectorAll('.btn button');
+
+botones.forEach(e => {
+    e.addEventListener('click', (btn)=>{
+        const btnValor = btn.target.innerHTML;
+        
+        if(isNaN(btnValor) === false){
+            if(rpta.innerHTML.length < 16){
+                rpta.innerHTML += eval(btnValor);
+            }
+
+        }else if(btnValor === '='){
+            if(rpta.innerHTML !== '' && isNaN(rpta.innerHTML.substr(-1)) === false){
+                rpta.innerHTML = eval(rpta.innerHTML);
+            }
+
+        }else if(btnValor === 'CLS'){
+            rpta.innerHTML = '';
+
+        }else{
+            if(isNaN(rpta.innerHTML.substr(-1)) === false){
+                rpta.innerHTML += btnValor;
+            }else{
+                rpta.innerHTML = rpta.innerHTML.substr(0, rpta.innerHTML.length -1) + btnValor;
+            }
+        }    
+    });
+})
